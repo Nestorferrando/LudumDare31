d17d886e8c6652377b904dc6112f73808cddd749
Nestorferrando <nestor.ferrando@akamon.me>
2014-12-07 17:08:43 +0100

    aadded missing files

---
+++ proyecto/Assets/src/model/duel/InmigrantState.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+    public enum InmigrantState
+    { idle,walking,crouching,shooting,dying,cocking
+    }
+

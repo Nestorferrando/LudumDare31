3f80a965e560e07723a3204214a5fdc6ddc61a7c
llikdowk <guillermofm7@gmail.com>
2014-12-07 10:44:17 +0100

    keyboard input init

---
+++ proyecto/Assets/src/input/InputResult.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace Assets.scripts
+{
+    class InputResult
+    {
+        private Dictionary<InputValues, Boolean> values=new Dictionary<InputValues, bool>();
+
+        public InputResult()
+        {
+           
+        }
+
+        public InputResult set(InputValues input, bool value)
+        {
+           this.values.Add(input,value);
+            return this;
+        }
+
+        public bool get(InputValues input)
+        {
+            bool isPressed;
+
+            this.values.TryGetValue(input, out isPressed);
+            return isPressed;
+        }
+
+
+    }
+}
---
+++ proyecto/Assets/src/input/InputUtils.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets.scripts.input
+{
+    internal class InputUtils
+    {
+        public const float tapTimeWindow_sec = 0.1f;
+
+        private static readonly Dictionary<InputValues, float> values = new Dictionary<InputValues, float>();
+
+
+        public static InputResult readInput()
+        {
+            var result = new InputResult();
+
+
+            result.set(InputValues.SELECT, Input.GetKeyDown("space"));
+
+            return result;
+        }
+
+        private static void getInputPulse(string keycode, InputValues value, InputResult result)
+        {
+           
+            if (Input.GetKey(keycode))
+            {
+                float currentTime = Time.realtimeSinceStartup;
+                float lastTimePressed;
+                values.TryGetValue(value, out lastTimePressed);
+                if (lastTimePressed != 0)
+                {
+                    values[value] = currentTime;
+                    if (currentTime - lastTimePressed > tapTimeWindow_sec)
+                    {
+                        result.set(value, Input.GetKey(keycode));
+                    }
+                }
+                else
+                {
+                    result.set(value, Input.GetKey(keycode));
+                    values[value]= currentTime;
+                }
+            }
+            else
+            {
+                values.Remove(value);
+            }
+        }
+    }
+}

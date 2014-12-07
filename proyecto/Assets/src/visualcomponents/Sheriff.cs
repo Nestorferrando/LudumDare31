270f0d4f240d030d52f1dc1920e165bcbf9a4dc9
llikdowk <guillermofm7@gmail.com>
2014-12-07 14:03:11 +0100

    Dialog fading in and out (buggy)

---
+++ proyecto/Assets/src/visualcomponents/Sheriff.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+
+public class Sheriff : MonoBehaviour
+{
+
+    private SheriffModel model=new SheriffModel(Role.BusinessMan,Role.Criminal);
+  
+    private PersonState currentState;
+    private PersonState proposedState;
+    public Boolean facingLeft;
+
+
+
+    public Sheriff()
+    {
+        currentState = PersonState.walking;
+        proposedState = PersonState.idle;
+    }
+
+    public SheriffModel Model
+    {
+        get { return model; }
+        set { model = value; }
+    }
+
+
+    public void moveInstantlyToPosition(Vector2 position)
+    {  
+        rigidbody2D.transform.position = position;
+        proposedState = PersonState.idle;
+        this.model.HiddenBehindBarrel = false;
+    }
+
+    public PersonState CurrentState
+    {
+        get { return currentState; }
+    }
+
+
+    public void performShootAnimation(Vector2 position)
+    {
+        proposedState = PersonState.shooting;
+    }
+
+    public void performDieAnimation(Vector2 position)
+    {
+        proposedState = PersonState.dying;
+    }
+
+
+    public void crouch()
+    {
+        this.model.HiddenBehindBarrel = true;
+        proposedState = PersonState.crouching; 
+    }
+
+
+    public void standUp()
+    {
+        this.model.HiddenBehindBarrel = false;
+        proposedState = PersonState.idle; 
+    }
+
+
+    private void FixedUpdate()
+    {
+
+        //set animation
+
+        if (facingLeft)
+        {
+            Vector3 theScale = transform.localScale;
+            theScale.x = -1;
+            transform.localScale = theScale;
+        }
+        else
+        {
+            Vector3 theScale = transform.localScale;
+            theScale.x = 1;
+            transform.localScale = theScale;
+        }
+
+
+        if (proposedState != currentState)
+        {
+            currentState = proposedState;
+            GetComponent<Animator>().Play(model.Role.ToString() + "_" + proposedState.ToString());
+        }
+
+    }
+
+
+
+    // Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
--- proyecto/Assets/src/visualcomponents/ui_dialog/UIDialog.cs
+++ proyecto/Assets/src/visualcomponents/ui_dialog/UIDialog.cs
@@ -5,31 +5,57 @@ using UnityEngine.UI;
 
 public class UIDialog : MonoBehaviour
 {
-
+    public Font font;
+    public int fontSize = 12;
+    [Range(0, 1f)] public float fadeSpeed = .15f;
     private Text dialog;
     private float delay;
     private float startTime;
+    private Image img;
+    private GameObject child;
+
+
 
 	void Start ()
 	{
-
-	    dialog = gameObject.AddComponent<Text>();
-	    delay = 1000; //ms
+	    child = GameObject.Find("Text");
+	    dialog = child.AddComponent<Text>();
+	    dialog.font = font;
+	    dialog.fontSize = fontSize;
+        delay = 1; //s
 
 	    dialog.text = Dialog.getDialog(Role.BusinessMan);
 	    startTime = Time.fixedTime;
 
-        Debug.Log("DESTROY");
-	}
+
+	    img = this.GetComponent<Image>();
+
+	    fadeOut();
+    }
 	
 	// Update is called once per frame
 	void Update () {
 	    if (Time.fixedTime > startTime + delay)
 	    {
-
-	        Destroy(this);
+	        fadeIn();
+            DestroyObject(child.GetComponent<Text>());
+            DestroyObject(this);
 	    }
 
-        Destroy(gameObject.GetComponent<UIDialog>());
 	}
+
+    void setDimension()
+    {
+        
+    }
+
+    void fadeIn()
+    {
+        img.CrossFadeAlpha(0f, fadeSpeed, false); // sec
+    }
+
+    void fadeOut()
+    {
+        img.CrossFadeAlpha(100f, fadeSpeed, false); // sec
+    }
 }
